b46b84d347f2581e97a55c3d7bd5c6a538b9cf47
PB <xachineon@gmail.com>
2026-02-18 11:17:45 +0530

    first commit

---
+++ Day1/bandNameGen.py
@@ -0,0 +1,4 @@
+print("Welcome to band name generator.")
+city = input("What's the name of your city?")
+pet = input("What's the name of your pet?")
+print("Your band name could be "+city+" "+pet)
---
+++ Day1/task.py
@@ -0,0 +1,12 @@
+print("Hello World\nHello world\nHello world")
+print("Hello"+" Man")
+
+#input("What is your age?\n")
+
+print("Hello "+input("What is your name?\n") +"!")
+
+age = input("What is your age? ")
+print("Your age is "+age+"!")
+
+country = input("What is your country?\n")
+print("Your country has "+ str(len(country))+ " letters.")
