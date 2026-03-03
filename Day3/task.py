bbffe2e39b8acaf4bd2b42e4fb92e8ba4c236b4a
PB <xachineon@gmail.com>
2026-03-03 11:41:33 +0530

    bill change

--- Day3/task.py
+++ Day3/task.py
@@ -5,9 +5,20 @@ height = int(input("Enter height:: "))
 if height>120:
     print("Okay")
     age = int(input("Enter age:: "))
-    if age>=18:
-        print("Pay $12")
+    if age >=12:
+        print("$5")
+        bill = 5
+    elif age>=18:
+        print("$12")
+        bill = 12
     else:
-        print("Pay $7")
+        print("$7")
+        bill = 7
+    want_photo = input("Want photo taken? ")
+
+    if want_photo == "y":
+        bill += 3
+    
+    print(f"You must pay {bill}")
 else:
     print("No")
