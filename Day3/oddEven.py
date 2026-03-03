7f011c66c9ebc5fc1c298ef7e97aad0623ee06a4
PB <xachineon@gmail.com>
2026-03-03 11:25:09 +0530

    More commits

---
+++ Day3/oddEven.py
@@ -0,0 +1,8 @@
+print("Welcome to odd even checker.")
+
+num = int(input("Enter an integer:: "))
+
+if num%2 == 0:
+    print("You've entered an even number.")
+else:
+    print("You've entered an odd number.")
--- Day3/task.py
+++ Day3/task.py
@@ -4,5 +4,10 @@ height = int(input("Enter height:: "))
 
 if height>120:
     print("Okay")
+    age = int(input("Enter age:: "))
+    if age>=18:
+        print("Pay $12")
+    else:
+        print("Pay $7")
 else:
     print("No")
